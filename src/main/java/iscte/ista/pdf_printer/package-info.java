4a7191f900f7ce33311f103e9f1ecc5da3c9558e
LETI-122627 <gongas.gui575@gmail.com>
2025-10-09 15:47:22 +0100

    Adicionado o package-info.java

---
+++ src/main/java/iscte/ista/pdf_printer/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Pacote para funcionalidades de impressão de PDF.
+ * Inclui componentes de UI, lógica de negócio e acesso a dados para impressoras PDF.
+ */
+@NullMarked
+package iscte.ista.pdf_printer;
+
+import org.jspecify.annotations.NullMarked;
